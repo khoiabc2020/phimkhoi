303d0635d01ecf9d1a4ec65e5e8135d47bdcaec4
Le Huy Khoi <khoilehuy318@gmail.com>
2026-03-04 10:31:48 +0700

    chore: force load nextauth environment variables into pm2 process to resolve 500 auth endpoint error

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,6 +10,8 @@ module.exports = {
         NODE_ENV: "production",
         PORT: "3000",
         NODE_OPTIONS: "--max_old_space_size=2048",
+        NEXTAUTH_URL: "https://khoiphim.io.vn",
+        NEXTAUTH_SECRET: "khoiphim-secret-key-123",
       },
       max_memory_restart: "512M",
     },
