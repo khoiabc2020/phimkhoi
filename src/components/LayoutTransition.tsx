119a66cb64f5d3b32b1a5dca77524977a2427726
Le Huy Khoi <khoilehuy318@gmail.com>
2026-03-23 14:25:17 +0700

    sync: update web + optimize

--- src/components/LayoutTransition.tsx
+++ src/components/LayoutTransition.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
-
 export default function LayoutTransition({ children }: { children: React.ReactNode }) {
-    const pathname = usePathname();
-
-    return (
     return <div className="w-full flex-1">{children}</div>;
-    );
 }
