7dd318b7e8d5720129e3bb045d084ad6682c50b3
Le Huy Khoi <khoilehuy318@gmail.com>
2026-03-23 21:49:55 +0700

    sync: update web + optimize

--- src/components/VersionWatcher.tsx
+++ src/components/VersionWatcher.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-export default function VersionWatcher() {
+export default function VersionWatcher(): null {
     const router = useRouter();
 
     useEffect(() => {
